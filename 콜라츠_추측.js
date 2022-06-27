586c403e0dea57cfd356daec7ddeb4cddaf49138
yuriiiiiiiiiii <angel221110@gmail.com>
2022-06-28 01:05:48 +0900

    반복되는 조건 줄이기

---
+++ 콜라츠_추측.js
@@ -0,0 +1,8 @@
+function solution(num) {
+  let count = 0;
+  while (num !== 1 && count !== 500) {
+    num % 2 === 0 ? (num = num / 2) : (num = num * 3 + 1);
+    count++;
+  }
+  return num === 1 ? count : -1;
+}
